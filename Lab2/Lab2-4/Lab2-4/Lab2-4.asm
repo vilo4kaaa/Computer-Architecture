87e45ee6cc121a7e1f67a34af3086e767399c2e1
vilo4kaaa <vika.masyuk@gmail.com>
2021-10-23 18:43:20 +0300

    Lab2-4 v10: add 1,2,3-check

--- Lab2/Lab2-4/Lab2-4/Lab2-4.asm
+++ Lab2/Lab2-4/Lab2-4/Lab2-4.asm
@@ -4,10 +4,17 @@
 .stack 4096
 
 .data
-n DWORD 12115
-repeats DWORD 0
+n DWORD 122333
+k DWORD 2
+one_repeats DWORD 0
+two_repeats DWORD 0
+three_repeats DWORD 0
 one DWORD 1
+two DWORD 2
+three DWORD 3
 ten DWORD 10
+
+more BYTE 0
 
 ExitProcess proto, dwExitCode:dword
 
@@ -25,14 +32,47 @@ jmp check
 
 check:
 cmp edx, one
-je inc_repeats
+je inc_one_repeats
+
+cmp edx, two
+je inc_two_repeats
+
+cmp edx, three
+je inc_three_repeats
+
+
+inc_one_repeats:
+inc one_repeats
 jmp BeginWhile
 
-inc_repeats:
-inc repeats
+inc_two_repeats:
+inc two_repeats
+jmp BeginWhile
+
+inc_three_repeats:
+inc three_repeats
 jmp BeginWhile
 
 EndWhile:
+
+mov eax, one_repeats
+cmp eax, k
+jg a
+
+mov eax, two_repeats
+cmp eax, k
+jg a
+
+mov eax, three_repeats
+cmp eax, k
+jg a
+jmp EndOfIf
+
+a:
+mov more, 1
+jmp EndOfIf
+
+endOfIf:
 
 invoke ExitProcess,0
 
